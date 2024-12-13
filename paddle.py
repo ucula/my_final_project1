9926a4958e90ba809750c3d29dcecf9a29ad9d0c
Sethtatad KIJKANJANARAT <seth.kij@gmail.com>
2024-12-13 21:44:18 +0700

    rewrite the paddle to my liking

--- paddle.py
+++ paddle.py
@@ -1,35 +1,34 @@
-class Paddle:
-    def __init__(self, width, height, color, my_turtle):
-        self.width = width
-        self.height = height
-        self.location = [0, 0]
-        self.color = color
-        self.my_turtle = my_turtle
-        self.my_turtle.penup()
-        self.my_turtle.setheading(0)
-        self.my_turtle.hideturtle()
+import turtle
 
-    def set_location(self, location):
-        self.location = location
-        self.my_turtle.goto(self.location[0], self.location[1])
 
-    def draw(self):
-        self.my_turtle.color(self.color)
-        self.my_turtle.goto(self.location[0], self.location[1] - self.height/2)
-        self.my_turtle.forward(self.width/2)
-        self.my_turtle.pendown()
-        self.my_turtle.begin_fill()
-        for _ in range(2):
-            self.my_turtle.left(90)
-            self.my_turtle.forward(self.height)
-            self.my_turtle.left(90)
-            self.my_turtle.forward(self.width)
-        self.my_turtle.end_fill()
-        self.my_turtle.penup()
-        self.my_turtle.goto(self.location[0], self.location[1])
+class Paddle(turtle.Turtle):
+    def __init__(self):
+        super().__init__()  # I do this because I don't want to type self.paddle.(Method)
+        self.penup()
+        self.goto(0, -250)  # the initial spot for the paddle (x, y)
+        self.shape("square")
+        self.color("white")  # set the color of the paddle
+        self.shapesize(1, 5)  # set width and length of the paddle
+        self.control()
 
-    def clear(self):
-        self.my_turtle.clear()
+    # tell the paddle to move to the left
+    def move_left(self):
+        x = self.xcor()
+        x -= 50  # set how far the paddle can move to the left
+        if x < -350:  # check if it goes over border, if not, do the following methods
+            x = -350
+        self.setx(x)
 
-    def __str__(self):
-        return "paddle"
+    # move the paddle to move to the right
+    def move_right(self):
+        x = self.xcor()
+        x += 50  # set how far the paddle can move to the right
+        if x > 350:  # check if it goes over border, if not, do the following methods
+            x = 350
+        self.setx(x)
+
+    # assign keys to move paddle
+    def control(self):
+        self.screen.listen()
+        self.screen.onkeypress(self.move_left, "Left")  # assign "Left key" to the move left function
+        self.screen.onkeypress(self.move_right, "Right")  # assign "Right key" to the move right function
