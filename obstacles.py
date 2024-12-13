bfb084840d51912a28693e1a83ff1cbb62a261a7
Sethtatad KIJKANJANARAT <seth.kij@gmail.com>
2024-12-14 00:20:17 +0700

    make a proper obstacle class

--- obstacles.py
+++ obstacles.py
@@ -1,35 +1,12 @@
-class Obstacles:
-    def __init__(self, width, height, color, my_turtle):
-        self.width = width
-        self.height = height
-        self.color = color
-        self.location = [0, 0]
-        self.my_turtle = my_turtle
-        self.my_turtle.penup()
-        self.my_turtle.setheading(0)
-        self.my_turtle.hideturtle()
+import turtle
+import random
 
-    def set_location(self, location):
-        self.location = location
-        self.my_turtle.goto(self.location[0], self.location[1])
-
-    def draw(self):
-        self.my_turtle.color(self.color)
-        self.my_turtle.goto(self.location[0], self.location[1] - self.height/2)
-        self.my_turtle.forward(self.width/2)
-        self.my_turtle.pendown()
-        self.my_turtle.begin_fill()
-        for _ in range(2):
-            self.my_turtle.left(90)
-            self.my_turtle.forward(self.height)
-            self.my_turtle.left(90)
-            self.my_turtle.forward(self.width)
-        self.my_turtle.end_fill()
-        self.my_turtle.penup()
-        self.my_turtle.goto(self.location[0], self.location[1])
-
-    def clear(self):
-        self.my_turtle.clear()
-
-    def __str__(self):
-        return "obstacles"
+class Obstacle(turtle.Turtle):
+    def __init__(self, x, y):
+        super().__init__()
+        self.shape("square")
+        self.color(random.choice(["yellow", "purple", "blue"]))
+        self.speed(0)
+        self.shapesize(stretch_wid=1, stretch_len=3.2)
+        self.penup()
+        self.goto(x, y)
