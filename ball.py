9e30cdcc6135a0f7190b5205579e8510d1783da3
Sethtatad KIJKANJANARAT <seth.kij@gmail.com>
2024-12-13 21:47:10 +0700

    rewrite ball

--- ball.py
+++ ball.py
@@ -1,51 +1,10 @@
 import turtle
-import math
 
-class Ball:
-    def __init__(self, size, x, y, vx, vy, color):
-        self.size = size
-        self.x = x
-        self.y = y
-        self.vx = vx
-        self.vy = vy
-        self.color = color
-        self.canvas_width = turtle.screensize()[0]
-        self.canvas_height = turtle.screensize()[1]
-
-    def draw_ball(self):
-        turtle.clear()
-        turtle.penup()
-        turtle.color(self.color)
-        turtle.fillcolor(self.color)
-        turtle.goto(self.x, self.y)
-        turtle.pendown()
-        turtle.begin_fill()
-        turtle.circle(self.size)
-        turtle.end_fill()
-
-    def bounce_off_vertical_wall(self):
-        self.vx = -self.vx
-
-    def bounce_off_horizontal_wall(self):
-        self.vy = -self.vy
-
-    def move(self, dt):
-        self.x += self.vx * dt
-        self.y += self.vy * dt
-
-    def time_to_hit_vertical_wall(self):
-        if self.vx > 0:
-            return (self.canvas_width - self.x - self.size) / self.vx
-        elif self.vx < 0:
-            return (self.canvas_width + self.x - self.size) / (-self.vx)
-        else:
-            return math.inf
-
-    def time_to_hit_horizontal_wall(self):
-        if self.vy > 0:
-            return (self.canvas_height - self.y - self.size) / self.vy
-        elif self.vy < 0:
-            return (self.canvas_height + self.y - self.size) / (-self.vy)
-        else:
-            return math.inf
-
+class Ball(turtle.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.color('red')
+        self.setheading(270)
+        self.vx = 1
+        self.vy = 1
