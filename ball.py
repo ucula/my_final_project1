433b69e947f88e4e88af2bba49ab7d62901275d4
Sethtatad KIJKANJANARAT <seth.kij@gmail.com>
2024-12-14 02:31:19 +0700

    ball.py almost finish

--- ball.py
+++ ball.py
@@ -6,9 +6,10 @@ class Ball(turtle.Turtle):
         self.shape('circle')
         self.color('red')
         self.penup()
-        self.vx = 0.25  # set the initial ball speed of xcor
-        self.vy = 0.25  # set the initial ball speed of ycor
+        self.vx = 1.5  # set the initial ball speed of xcor
+        self.vy = 1.5  # set the initial ball speed of ycor
 
+    # how this works is I tell the turtle to go to (the current cords + given parameter(how fast it can move))
     def move(self):
         self.goto(self.xcor() + self.vx, self.ycor() + self.vy)
 
@@ -16,16 +17,23 @@ class Ball(turtle.Turtle):
         # Check for collisions with the walls
         if self.xcor() > 390:
             self.setx(390)
-            self.vx *= -1
+            self.vx = -self.vx
 
         if self.xcor() < -390:
             self.setx(-390)
-            self.vx *= -1
+            self.vx = -self.vx
 
     def wall_bounce_vertical(self):
         if self.ycor() > 290:
             self.sety(290)
-            self.vy *= -1
+            self.vy = -self.vy
+
+        if self.ycor() < -290:
+            self.sety(-290)
+            self.vy = -self.vy
 
     def paddle_bounce(self):
+        self.vy = -self.vy
+
+    def obstacle_bounce(self):
         self.vy = -self.vy
