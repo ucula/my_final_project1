c212403d85a2317c8b1d07ce5aa0f5c3b5355bea
Sethtatad KIJKANJANARAT <seth.kij@gmail.com>
2024-12-13 23:28:52 +0700

    add a bounce mechanics

--- ball.py
+++ ball.py
@@ -5,6 +5,27 @@ class Ball(turtle.Turtle):
         super().__init__()
         self.shape('circle')
         self.color('red')
-        self.setheading(270)
-        self.vx = 1
-        self.vy = 1
+        self.penup()
+        self.vx = 0.25  # set the initial ball speed of xcor
+        self.vy = 0.25  # set the initial ball speed of ycor
+
+    def move(self):
+        self.goto(self.xcor() + self.vx, self.ycor() + self.vy)
+
+    def wall_bounce_horizontal(self):
+        # Check for collisions with the walls
+        if self.xcor() > 390:
+            self.setx(390)
+            self.vx *= -1
+
+        if self.xcor() < -390:
+            self.setx(-390)
+            self.vx *= -1
+
+    def wall_bounce_vertical(self):
+        if self.ycor() > 290:
+            self.sety(290)
+            self.vy *= -1
+
+    def paddle_bounce(self):
+        self.vy = -self.vy
