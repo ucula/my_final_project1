d4d697c51c24583108212a07a641dc6cccad75b1
Sethtatad KIJKANJANARAT <seth.kij@gmail.com>
2024-12-14 02:32:21 +0700

    obstacles.py done

--- obstacles.py
+++ obstacles.py
@@ -5,7 +5,7 @@ class Obstacle(turtle.Turtle):
     def __init__(self, x, y):
         super().__init__()
         self.shape("square")
-        self.color(random.choice(["yellow", "purple", "blue"]))
+        self.color(random.choice(["peru", "cyan", "gold1", "olivedrab"]))
         self.speed(0)
         self.shapesize(stretch_wid=1, stretch_len=3.2)
         self.penup()
