af0cc46850097aac2c3655e5dbba0372eb0592a4
Sethtatad KIJKANJANARAT <seth.kij@gmail.com>
2024-12-14 14:17:29 +0700

    add comments and reorganize some codes

--- ball.py
+++ ball.py
@@ -1,39 +1,23 @@
 import turtle
 
-class Ball(turtle.Turtle):
+
+class Ball(turtle.Turtle):  # Make this class a child of turtle class, there's a reason why I do this
     def __init__(self):
-        super().__init__()
+        super().__init__()  # Implement all turtle methods in
         self.shape('circle')
         self.color('red')
-        self.penup()
-        self.vx = 1.5  # set the initial ball speed of xcor
-        self.vy = 1.5  # set the initial ball speed of ycor
+        self.penup()  # No more line from ball_turtle
+        self.vx = 1.5  # Set the initial ball speed of xcor(increasing this value will make the game harder to play)
+        self.vy = 1.5  # Set the initial ball speed of ycor(increasing this value will make the game harder to play)
 
-    # how this works is I tell the turtle to go to (the current cords + given parameter(how fast it can move))
+    # How this works is I tell the turtle to go to (the current cords + given parameter(how fast it can move))
     def move(self):
         self.goto(self.xcor() + self.vx, self.ycor() + self.vy)
 
-    def wall_bounce_horizontal(self):
-        # Check for collisions with the walls
-        if self.xcor() > 390:
-            self.setx(390)
-            self.vx = -self.vx
+    # Change direction of the ball in xcor
+    def bounce_off_xcor(self):
+        self.vx = -self.vx
 
-        if self.xcor() < -390:
-            self.setx(-390)
-            self.vx = -self.vx
-
-    def wall_bounce_vertical(self):
-        if self.ycor() > 290:
-            self.sety(290)
-            self.vy = -self.vy
-
-        if self.ycor() < -290:
-            self.sety(-290)
-            self.vy = -self.vy
-
-    def paddle_bounce(self):
-        self.vy = -self.vy
-
-    def obstacle_bounce(self):
+    # Change direction of the ball in ycor
+    def bounce_off_ycor(self):
         self.vy = -self.vy
