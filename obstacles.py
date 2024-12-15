7a2c5dc13a6a9407c56166135e1db84432874a2a
Sethtatad KIJKANJANARAT <seth.kij@gmail.com>
2024-12-15 21:54:42 +0700

    add more variables

--- obstacles.py
+++ obstacles.py
@@ -8,8 +8,10 @@ class Obstacle(turtle.Turtle):
     """
     def __init__(self, x, y):
         super().__init__()
+        self.x = x
+        self.y = y
         self.penup()
         self.shape("square")
         self.color(random.choice(["peru", "cyan", "gold1", "olivedrab", "blue", "pink"]))
         self.shapesize(1, 3)
-        self.goto(x, y)
+        self.goto(self.x, self.y)
